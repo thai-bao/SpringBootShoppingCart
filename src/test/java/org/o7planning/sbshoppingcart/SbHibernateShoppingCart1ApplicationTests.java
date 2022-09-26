b27e78cb875831cec037272450138fc125d475fc
BaoNTT <baont.fbis@gmail.com>
2022-09-26 10:27:08 +0700

    First commit

---
+++ src/test/java/org/o7planning/sbshoppingcart/SbHibernateShoppingCart1ApplicationTests.java
@@ -0,0 +1,10 @@
+package org.o7planning.sbshoppingcart;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbHibernateShoppingCart1ApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
